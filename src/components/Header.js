d2a5c8a561474b768efc23f54923b212a0800221
helen burgess <heb264@gmail.com>
2018-03-02 11:26:08 +0800

    added more component files

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class Header extends React.Component{
+   render(){
+      return (
+         <header className="top">
+         <h1>Catch 
+         <span classname="of">of</span>
+         <span classname="the"> the </span>
+         Day</h1>
+         <h3 className="tagline">Fresh Seafood Market </h3>
+         </header>
+
+      );
+   }
+}
+
+export default Header;
