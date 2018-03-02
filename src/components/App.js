bddee95de8d5c6e9b96e1abb956027e1b529bc30
helen burgess <heb264@gmail.com>
2018-03-02 13:20:54 +0800

    adding load samples fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component{
   state = {
@@ -18,13 +19,18 @@ class App extends React.Component{
     this.setState({fishes});
   };
 
+  loadSampleFishes =() =>{
+    alert("loading fishes");
+    this.setState({fishes: sampleFishes});
+  }
+
    render(){
       return <div className="catch-of-the-day">
           <div className="menu">
             <Header tagline="Fresh Seafood Market" age="100" />
           </div>
           <Order />
-          <Inventory addFish={this.addFish}/>
+          <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
         </div>;
    }
 }
