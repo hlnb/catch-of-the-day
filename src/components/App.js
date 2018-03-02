53c04aeb5297ae268faf1250d7080373b5c933b7
helen burgess <heb264@gmail.com>
2018-03-02 13:14:05 +0800

    setting state

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,27 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component{
+  state = {
+    fishes:{},
+    order:{}
+
+  };
+  addFish = fish => {
+    //1. take a copy of the existing state
+    const fishes ={...this.state.fishes};
+    //add new fish to the fishes variable;
+    fishes[`fish${Date.now()}`] = fish;
+//3. set the new fishes object to state.
+    this.setState({fishes});
+  };
+
    render(){
       return <div className="catch-of-the-day">
           <div className="menu">
-            <Header tagline="Fresh Seafood Market" age="100"/>
+            <Header tagline="Fresh Seafood Market" age="100" />
           </div>
-
-          <Inventory />
           <Order />
+          <Inventory addFish={this.addFish}/>
         </div>;
    }
 }
