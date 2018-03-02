589cf26612cff4a6ba75617d0b5f379fadda0a69
helen burgess <heb264@gmail.com>
2018-03-02 12:21:13 +0800

    events, routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,34 @@
 import React, {Fragment} from 'react';
-
+import {getFunName } from '../helpers';
 class StorePicker extends React.Component {
-   render() {
-      return (
-         <Fragment>
-         <form action="" className="store-selector">
-         { /* making a comment*/}
-         <h2>Please Enter a Store</h2>
-         <input type="text" required placeholder="Store Name" />
-         <button type="submit"> Visit Store -> </button>
-         </form>
-         </Fragment>
-      );
-   }
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    //1. stor the form from submitting
+    event.preventDefault();
+    //2. get the text from tht input
+    const storename = this.myInput.value.value;
+    //3.change page to  /store/whatever-they-entered
+    this.props.history.push(`/store/${storename}`)
+  }
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.goToStore} className="store-selector">
+          {/* making a comment*/}
+          <h2>Please Enter a Store</h2>
+          <input
+            ref={this.myInput}
+            type="text"
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit"> Visit Store -> </button>
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default StorePicker;
