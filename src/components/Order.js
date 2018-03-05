16b2036e953d9073dc55fe8b470fd38b19f68541
helen burgess <heb264@gmail.com>
2018-03-05 10:23:19 +0800

    updated transitions

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,22 +7,27 @@ renderOrder = key =>{
    const fish = this.props.fishes[key];
    const count = this.props.order[key];
    const isAvailable = fish && fish.status ==='available';
+   const transitionOptions ={
+     classNames:"order",
+     key,
+     timeout: { enter: 500, exit: 500 }
+   };
    //makesure the fish is loaded before continuing.
    if(!fish)return null;
    if(!isAvailable){
       return (
-      <CSSTransition classNames="order" key={key} timeout={{enter: 250, exit: 250}}>
+      <CSSTransition {...transitionOptions}>
       <li key={key}>
       Sorry {fish ? fish.name: 'fish'} is no longer available!
       </li>
       </CSSTransition>
     );
    }
-   return <CSSTransition classNames="order" key={key} timeout={{ enter: 500, exit: 500 }}>
+   return <CSSTransition {...transitionOptions}>
        <li key={key}>
          <span>
            <TransitionGroup component="span" className="count">
-             <CSSTransition classNames="count" key={count} timeout={{enter: 5000, exit: 5000}}><span>{count}</span></CSSTransition>
+             <CSSTransition classNames="count" key={count} timeout={{enter: 500, exit: 500}}><span>{count}</span></CSSTransition>
            </TransitionGroup>
            lbs {fish.name}
            {formatPrice(count * fish.price)}
